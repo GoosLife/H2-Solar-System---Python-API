3877c17d01ba388cd1243f8f4b837cd142a3ca18
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2023-06-12 12:57:33 +0200

    Made externally visible

--- Python API/python_API.py
+++ Python API/python_API.py
@@ -1,5 +1,9 @@
 from application import app
 import router
 
+import subprocess
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
