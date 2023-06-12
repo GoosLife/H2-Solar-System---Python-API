2320639c2dc7b475a4e1c1c61c669086015b4c40
GoosLife <72657308+GoosLife@users.noreply.github.com>
2023-06-12 17:51:45 +0200

    Hologram POST endpoint created

--- Python API/python_API.py
+++ Python API/python_API.py
@@ -1,9 +1,5 @@
 from application import app
 import router
 
-import subprocess
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- Python API/router.py
+++ Python API/router.py
@@ -1,13 +1,15 @@
 from application import app
 from models import planet, hologram
 from database import db
-from sqlalchemy import select
-
-from flask import jsonify
+from flask import jsonify, request
+from sqlalchemy import text
+from datetime import datetime
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+## PLANETS
 
 @app.route('/planets')
 def get_planets():
@@ -25,34 +27,61 @@ def get_planets():
 
     return jsonify(planet_data)
 
-@app.route('/hologram')
-def get_hologram():
-    statement = select(hologram.Hologram.planetId).filter_by(machineId=1)
-    planetId = db.session.execute(statement)
-    return planetId
-
 @app.route('/planets/<int:planet_id>')
 def get_planet(planet_id):
     queriedPlanet = planet.Planet.query.get_or_404(planet_id);
     return str(queriedPlanet)
 
-@app.route('/hologram/<int:current_planet_id>')
-def set_hologram_planet(current_planet_id):
-    new_hologram = hologram.Hologram(current_planet_id, 1) # TODO: Generate unique machine ID
-    
-    try:
-        db.session.add(new_hologram)
-        db.session.commit()
-        return 'Hologram created successfully: ' + str(new_hologram)
-    except Exception as e:
-        db.session.rollback()
+## HOLOGRAM
+
+# Get
+@app.route('/hologram/<int:machine_id>')
+def get_hologram(machine_id):
+    hologram_entry = hologram.Hologram.query.filter_by(machineId=machine_id).first()
+
+    if hologram_entry is not None:
+        planetId = hologram_entry.planetId
+        return str(planetId)
+
+    return "No hologram found for id " + machine_id
+
+
+
+# Set hologram
+@app.route('/hologram', methods=['POST'])
+def set_hologram_planet():
+    if request.content_type == 'application/json':
+        new_planet_id = request.json['planet_id']
+        new_machine_id = 1  # TODO: Generate unique machine ID
         
+        # Upsert hologram
+
         try:
-            db.session.execute(db.insert(hologram.Hologram),
-                               [{
-                                   "PlanetID":new_hologram.planetId,
-                                   "MachineID":new_hologram.machineId
-                                   }])
-            return 'Hologram created successfully: ' + str(new_hologram)
+            existing_hologram = hologram.Hologram.query.filter_by(machineId=new_machine_id).first()
+
+            # Update hologram
+
+            if existing_hologram:
+                existing_hologram.planetId = new_planet_id
+                db.session.commit()
+                return 'Hologram updated successfully: ' + str(existing_hologram)
+            
+            # Insert hologram
+
+            else:
+                new_hologram = hologram.Hologram(planetId=new_planet_id, machineId=new_machine_id)
+                db.session.add(new_hologram)
+                db.session.commit()
+                return 'Hologram created successfully: ' + str(new_hologram)
+        
+        # Failed to upsert hologram
+
         except Exception as e:
-            return "Failed to create hologram: " + str(e)
+            db.session.rollback()
+            print('\n\n~~~SQL ERROR~~~\n\n' + str(datetime.now()) + '\n\n' + str(e) + "\n\n~~~SQL ERROR END~~~\n\n")
+            return 'Failed to create or update hologram.'
+    else:
+        
+        # Invalid request body
+
+        return 'Request body must be JSON'
